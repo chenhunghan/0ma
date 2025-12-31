f0b05fac078e46f6a9a5ef27477eaff850343bf7
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2025-12-31 19:56:33 +0200

    refactor: Adjust ResizableLayout to render zero-sized panels and conditionally hidden handles when no columns are present.

--- src/components/ResizableLayout.tsx
+++ src/components/ResizableLayout.tsx
@@ -17,20 +17,23 @@ export function ResizableLayout({ columns, bottom }: ResizableLayoutProps) {
     return (
         <ResizablePanelGroup direction="vertical">
             {/* Top Section: Contains the columns (grid vertically on mobile, horizontally on desktop) */}
-            {columns.length > 0 ? <ResizablePanel defaultSize={40} minSize={10}>
-                <ResizablePanelGroup direction={isMobile ? "vertical" : "horizontal"}>
-                    {columns.map((column, index) => (
-                        <Fragment key={index}>
-                            <ResizablePanel defaultSize={100 / columns.length} minSize={10}>
-                                {column}
-                            </ResizablePanel>
-                            {index < columns.length - 1 && <ResizableHandle />}
-                        </Fragment>
-                    ))}
-                </ResizablePanelGroup>
-            </ResizablePanel> : null}
+            {columns.length > 0 ?
+                <ResizablePanel defaultSize={40} minSize={10}>
+                    <ResizablePanelGroup direction={isMobile ? "vertical" : "horizontal"}>
+                        {columns.map((column, index) => (
+                            <Fragment key={index}>
+                                <ResizablePanel defaultSize={100 / columns.length} minSize={10}>
+                                    {column}
+                                </ResizablePanel>
+                                {index < columns.length - 1 && <ResizableHandle />}
+                            </Fragment>
+                        ))}
+                    </ResizablePanelGroup>
+                </ResizablePanel> :
+                <ResizablePanel defaultSize={0}></ResizablePanel>
+            }
 
-            {columns.length > 0 ? <ResizableHandle /> : null}
+            <ResizableHandle className={columns.length > 0 ? "" : "hidden"} />
 
             {/* Bottom Section */}
             <ResizablePanel defaultSize={columns.length > 0 ? 60 : 100} minSize={8}>
