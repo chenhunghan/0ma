9003a042c6cd6c405d19edcd8e1b205ff7af65fd
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2026-01-03 16:47:51 +0200

    refactor: Remove client-side default Lima config and its use as initialData.

--- src/hooks/useDefaultK0sLimaConfig.ts
+++ src/hooks/useDefaultK0sLimaConfig.ts
@@ -2,22 +2,6 @@ import { useQuery } from "@tanstack/react-query";
 import { invoke } from "@tauri-apps/api/core";
 import { LimaConfig } from "../types/LimaConfig";
 
-/**
- * Placeholder default config in case fetching from backend fails
- */
-export const ClIENT_DEFAULT_CONFIG: LimaConfig = {
-  vmType: "",
-  cpus: 0,
-  memory: "",
-  disk: "",
-  images: [],
-  mounts: [],
-  minimumLimaVersion: "",
-  copyToHost: [],
-  provision: [],
-  probes: []
-}
-
 export function useDefaultK0sLimaConfig(
   instanceName: string,
   installHelm: boolean = true,
@@ -40,7 +24,6 @@ export function useDefaultK0sLimaConfig(
       return config;
     },
     enabled: !!instanceName, // Only fetch if instanceName is provided
-    initialData: ClIENT_DEFAULT_CONFIG,
   });
 
   return {
