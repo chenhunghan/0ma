9b7c520bdbfd4e310a779f0e89b06fc15e9eb7bb
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2026-01-04 16:09:37 +0200

    feat: always render Lima config columns by removing mobile-specific conditional logic

--- src/components/LimaConfigTabContent.tsx
+++ src/components/LimaConfigTabContent.tsx
@@ -3,26 +3,22 @@ import { TabsContent } from "./ui/tabs";
 import { LimaConfigResourceColumn } from "./LimaConfigResourceColumn";
 import { LimaConfigSystemColumn } from "./LimaConfigSystemColumn";
 import { LimaConfigAutomationColumn } from "./LimaConfigAutomationColumn";
-import { useIsMobile } from "src/hooks/useMediaQuery";
 import { LimaConfigEditor } from "./LimaConfigEditor";
 
 export function LimaConfigTabContent({ tabValue }: { tabValue: string }) {
-    const isMobile = useIsMobile()
     return (
         <TabsContent value={tabValue} className="h-full">
             <ResizableLayout
                 autoSaveId="lima-config-tabs-content"
-                columns={isMobile ? [] : [
-                    <ColunmWrapper key="1">
-                        <LimaConfigResourceColumn />
-                    </ColunmWrapper>,
-                    <ColunmWrapper key="2">
-                        <LimaConfigSystemColumn />
-                    </ColunmWrapper>,
-                    <ColunmWrapper key="3">
-                        <LimaConfigAutomationColumn />
-                    </ColunmWrapper>,
-                ]}
+                columns={[<ColunmWrapper key="1">
+                    <LimaConfigResourceColumn />
+                </ColunmWrapper>,
+                <ColunmWrapper key="2">
+                    <LimaConfigSystemColumn />
+                </ColunmWrapper>,
+                <ColunmWrapper key="3">
+                    <LimaConfigAutomationColumn />
+                </ColunmWrapper>]}
                 bottom={<LimaConfigEditor />}
             />
         </TabsContent>
